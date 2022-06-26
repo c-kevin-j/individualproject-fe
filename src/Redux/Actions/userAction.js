63ec06c6f5825a442ce138a5ab61b4311630de2c
c-kevin-j <kevin_julius@srwasia.com>
2022-06-26 13:53:44 +0700

    remove console.log

--- src/Redux/Actions/userAction.js
+++ src/Redux/Actions/userAction.js
@@ -1,36 +1,7 @@
 
-// import {GET_USERS, USERS_ERROR} from '../types'
-import { API_URL } from '../../../helper'
-import Axios from 'axios'
-
-// export const getUsers = () => async dispatch => {
-    
-//     try{
-//         const res = await Axios.get(`${API_URL}/users`)
-//         dispatch( {
-//             type: "GET_USERS",
-//             payload: res.data
-//         })
-//     }
-//     catch(error){
-//         dispatch( {
-//             type: "USERS_ERROR",
-//             payload: error,
-//         })
-//     }
-
-// }
-
-// export const saveUserAction = (data) => {
-//   console.log("DATA DARI COMPONENT UI",data)
-//   return {
-//     type: "ADD_USER",
-//     payload: data
-//   }
-// }
 
 export const loginAction = (data) => {
-    console.log("DATA DARI COMPONENT UI", data)
+    // console.log("DATA DARI COMPONENT UI", data)
     return {
       type: "LOGIN_SUCCESS",
       payload: data
@@ -45,7 +16,6 @@ export const logoutAction = () => {
   }
 
 export const editUser = (data) => {
-    console.log("DATA DARI COMPONENT UI", data)
     return {
       type: "EDIT_PROFILE",
       payload: data
--- src/Redux/Reducers/usersReducer.js
+++ src/Redux/Reducers/usersReducer.js
@@ -21,13 +21,10 @@ export default function usersReducer (state = initialState, action){
     switch(action.type){
 
         case "LOGIN_SUCCESS":
-            console.log("DAPAT DATA DARI ACTION", action.payload)
-            // console.log({ ...state, ...action.payload })
         return { user:{...state, ...action.payload} };
         case "LOGOUT":
             return initialState;
         case "EDIT_PROFILE":
-            console.log("form edit",action.payload)
             return { ...state, ...action.payload }
         default: return state
     }
