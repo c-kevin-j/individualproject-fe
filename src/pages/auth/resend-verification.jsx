476141a0d742a2530e458df05e518123522a87c4
c-kevin-j <kevin_julius@srwasia.com>
2022-06-26 13:56:36 +0700

    add resend verification page

---
+++ src/pages/auth/resend-verification.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { API_URL } from "../../../helper";
+
+const VerifyAccountPage = () => {
+  const handleReverify = async () => {
+    let token = localStorage.getItem("tokenIdUser");
+    try {
+      let res = await axios.get(`${API_URL}/users/verify/send`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.data.success) {
+        alert(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="px-10 md:px-32 lg:px-48 xl:px-80 pt-5 text-center space-y-6">
+        <div>Akun anda belum diverifikasi</div>
+        <div>
+          <button type="button" className="btn" onClick={handleReverify}>
+            Re-send Verification
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+VerifyAccountPage.layout = "L1";
+
+export default VerifyAccountPage;
