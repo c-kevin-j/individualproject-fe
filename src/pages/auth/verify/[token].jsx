5afdcf7c9c2cea30b20b0e464b0166e83b6ebd5e
c-kevin-j <kevin_julius@srwasia.com>
2022-06-26 13:57:41 +0700

    auto verify and route to home

--- src/pages/auth/verify/[token].jsx
+++ src/pages/auth/verify/[token].jsx
@@ -1,48 +1,57 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { API_URL } from "../../../../helper";
+import Swal from 'sweetalert2'
 
 const VerifyAccountPage = () => {
-
   const router = useRouter();
-  const { token }=router.query;
-  const { user } = useSelector((state)=>{
-      return{
-        user: state.usersReducer.user
-      }
-    })
+  const { token } = router.query;
+  const { user } = useSelector((state) => {
+    return {
+      user: state.usersReducer.user,
+    };
+  });
 
   const handleVerify = async () => {
-    try{
-      console.log(user)
-      let res = await axios.patch(`${API_URL}/users/verify`, {}, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+    try {
+      console.log(user);
+      let res = await axios.patch(
+        `${API_URL}/users/verify`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       // menyimpan ulang data
       localStorage.setItem("tokenIdUser", res.data.token);
       if (res) {
-        alert("berhasil verifikasi")
-        router.push("/")
+        Swal.fire({
+          title: 'Success!',
+          text: 'Your account has been verified',
+          icon: 'success',
+          confirmButtonText: 'Continue'
+        })
+        router.push("/");
       }
-    } catch(error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
+  React.useEffect(() => {
+    handleVerify();
+  }, [token]);
 
   return (
-    <>
-      <div className="px-10 md:px-32 lg:px-48 xl:px-80 pt-5">
-        Akun anda belum diverifikasi
-        <div>
-          <button type="button" className="btn" onClick={handleVerify}>verifikasi</button>
-        </div>
-      </div>
-    </>
+    <div className="px-10 md:px-32 lg:px-48 xl:px-80 pt-5 text-center space-y-4">
+      <div className="flex justify-center"><FaSpinner className="icon-spin" size={56}/></div>
+      <div>Verifying Your Account...</div>
+    </div>
   );
 };
 
