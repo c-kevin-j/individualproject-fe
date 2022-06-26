7681e08079ec1c76613ac58a4ec9289e4ff673f6
c-kevin-j <kevin_julius@srwasia.com>
2022-06-26 13:55:03 +0700

    add infinite scroll to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import axios from "axios";
 import { API_URL } from "../../helper";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export const getServerSideProps = async (ctx) => {
   try {
-    // // API JSON server
-    // let resUsers = await axios.get(`${API_URL}/users`);
-    // let resPosts = await axios.get(`${API_URL}/posts`);
-
-    // API Backend
     let resUsers = await axios.get(`${API_URL}/users/get`);
     let resPosts = await axios.get(`${API_URL}/posts/get/0`);
     return {
       props: {
         users: resUsers.data,
-        posts: resPosts.data,
+        posts: resPosts.data.posts,
       },
     };
   } catch (error) {
@@ -29,7 +25,8 @@ export const getServerSideProps = async (ctx) => {
 function HomePage(props) {
   // const dispatch = useDispatch();
   let { users } = props;
-  const [posts, setPosts]= useState(props.posts)
+  const [posts, setPosts] = useState(props.posts);
+  const [hasMore, setHasMore] = useState(true);
   const listInnerRef = React.useRef();
 
   const printPosts = () => {
@@ -46,16 +43,10 @@ function HomePage(props) {
                 <div className="flex bg-base-200">
                   <label className="btn btn-ghost btn-circle avatar mx-2 my-1 flex-none">
                     <Link href={`/profile?id=${users[idxUser].id}`}>
-                      {/* image jsonserver */}
-                      {/* <img
-                        className="avatar w-10 rounded-full"
-                        src={users[idxUser].profile_picture}
-                      /> */}
-
-                      {/* image backend */}
                       <img
                         className="avatar w-10 rounded-full"
                         src={`${API_URL}${users[idxUser].profile_picture}`}
+                        alt={`pict-${users[idxUser].username}`}
                       />
                     </Link>
                   </label>
@@ -72,16 +63,10 @@ function HomePage(props) {
               <div className="min-h-fit">
                 <Link href={`/post?id=${val.id}`}>
                   <figure>
-                    {/* Image json server */}
-                    {/* <img
-                      className="object-cover w-auto h-full"
-                      src={val.image}
-                    /> */}
-
-                    {/* Image backend */}
                     <img
                       className="object-cover w-auto h-full"
                       src={`${API_URL}${val.image}`}
+                      alt={`post-${users[idxUser].username}-${val.id}`}
                     />
                   </figure>
                 </Link>
@@ -97,83 +82,40 @@ function HomePage(props) {
     });
   };
 
-  const infiniteScroll = async () => {
-    if (listInnerRef.current) {
-      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
-      if (scrollTop + clientHeight === scrollHeight) {
-        let lastId = posts[posts.length-1].id
-        let res = await axios.get(`${API_URL}/posts/get/${lastId}`)
-        if (res){
-          let tempPosts = [...posts]
-          tempPosts.push(...res.data)
-          setPosts(tempPosts)
-          printPosts();
+  const getMorePosts = async () => {
+    try {
+      let lastId = posts[posts.length - 1].id;
+      let res = await axios.get(`${API_URL}/posts/get/${lastId}`);
+      if (res) {
+        if (!res.data.hasMore) {
+          setHasMore(!hasMore);
         }
+        let newPosts = [...res.data.posts];
+        setPosts((posts) => [...posts, ...newPosts]);
       }
+    } catch (error) {
+      console.log(error);
     }
   };
 
-
   return (
-    <div className="mx-auto px-6 lg:px-36 xl:px-96 pt-5" >
-      <div className="grid justify-items-center" onScroll={infiniteScroll()} ref={listInnerRef}>
-        {printPosts()}
-        {/* <div className="card rounded w-128">
-            <div className="card-body p-2 bg-base-300 ">
-              <div className="flex">
-                <label
-                  className="btn btn-ghost btn-circle avatar mx-2 my-auto flex-none"
-                >
-                  <img
-                    className="avatar w-10 rounded-full"
-                    src="https://api.lorem.space/image/face?hash=33791"
-                  />
-                </label>
-                <div className="mx-2 my-auto grow">
-                  @UsernameA
-                </div>
-                <div className="mx-2 my-auto text-sm text-slate-500">
-                  Created at
-                </div>
-              </div>
-              <Link href="/post">
-                <figure>
-                  <img
-                    className="object-cover"
-                    src="https://picsum.photos/500/500"
-                  />
-                </figure>
-              </Link>
-            </div>
-          </div> */}
-        {/* <div className="h-3" />
-          <div className="card rounded w-128">
-            <div className="card-body p-2 bg-base-300">
-              <div className="flex">
-                <label
-                  className="btn btn-ghost btn-circle avatar mx-2 my-auto flex-none"
-                >
-                  <img
-                    className="avatar w-10 rounded-full "
-                    src="https://api.lorem.space/image/face?hash=33791"
-                  />
-                </label>
-                <div className="mx-2 my-auto grow">@UsernameA</div>
-                <div className="mx-2 my-auto text-sm text-slate-500">
-                  Created at
-                </div>
-              </div>
-              <Link href="/post">
-                <figure>
-                  <img
-                    className="object-cover"
-                    src="https://picsum.photos/500/500"
-                    alt="Shoes"
-                  />
-                </figure>
-              </Link>
-            </div>
-          </div> */}
+    <div className="mx-auto px-6 lg:px-36 xl:px-96 pt-5">
+      <div className="grid justify-items-center">
+        {posts.length > 0 ? (
+          <InfiniteScroll
+            dataLength={posts.length}
+            next={getMorePosts}
+            hasMore={hasMore}
+            loader={<h3>Loading...</h3>}
+          >
+            {printPosts()}
+          </InfiniteScroll>
+        )
+          :
+          <div>
+            Nothing to see here...
+          </div>
+      }
       </div>
     </div>
   );
