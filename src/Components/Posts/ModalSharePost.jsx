423e5d7c27172f9c425fe0ecaf63c09d08ae0b5b
c-kevin-j <kevin_julius@srwasia.com>
2022-07-03 10:18:59 +0700

    create share modal

---
+++ src/Components/Posts/ModalSharePost.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Modal } from "react-daisyui";
+import { AiFillCopy } from "react-icons/ai";
+import { TiThumbsOk } from "react-icons/ti";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "react-share";
+
+const ModalSharePost = (props) => {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(props.shareUrl)
+  };
+  return (
+    <>
+      <Modal
+        className="bg-base-300 p-3 w-1/2 lg:w-1/3 rounded space-y-3"
+        open={props.visible}
+        onClickBackdrop={props.toggleVisible}
+      >
+        <div className="flex justify-center gap-x-2 p-1">
+          <FacebookShareButton url={props.shareUrl} quote={"hello"}>
+            <FacebookIcon size="40" round={true} />
+          </FacebookShareButton>
+          <WhatsappShareButton url={props.shareUrl} quote={"hello"}>
+            <WhatsappIcon size="40" round={true} />
+          </WhatsappShareButton>
+          <TwitterShareButton url={props.shareUrl} quote={"hello"}>
+            <TwitterIcon size="40" round={true} />
+          </TwitterShareButton>
+        </div>
+        <div className="input-group border rounded-lg">
+          <input
+            type="text"
+            className="w-full rounded focus:ring-none p-2 bg-white"
+            value={props.shareUrl}
+            readonly
+          />
+          <button type="button" className="bg-base-200" onClick={copyToClipboard}>
+            <AiFillCopy className="w-10 "/>
+          </button>
+        </div>
+        {/* <div className="flex h-3 relative -top-3 italic text-xs">
+         <TiThumbsOk size={40}/> url copied
+        </div> */}
+      </Modal>
+    </>
+  );
+};
+
+export default ModalSharePost;
