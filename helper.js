1002b4c1054824ea509d021631b1438091674570
c-kevin-j <kevin_julius@srwasia.com>
2022-07-03 14:38:27 +0700

    change helper url and keeplogin route

--- helper.js
+++ helper.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:5000"; // fake-backend
 // export const API_URL = "http://localhost:5500"; // backend
-export const API_URL = "https://393c-202-80-218-63.ap.ngrok.io"; // backend
+export const API_URL = "https://52e8-202-80-218-63.ap.ngrok.io"; // backend
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,15 +33,23 @@ function Navbar(props) {
 
   const keepLogin = async () => {
     let token = localStorage.getItem("tokenIdUser");
-    console.log(token)
+    console.log(token);
     if (token) {
       try {
         // backend
-        let res = await axios.get(`${API_URL}/users/login/keep`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        let res = await axios.post(
+          `${API_URL}/users/keepLogin`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              // "Access-Control-Allow-Origin": "*",
+              // "Access-Control-Allow-Methods":
+              //   "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            },
+          }
+        );
+        // res.headers["access-control-allow-origin"];
         localStorage.setItem("tokenIdUser", res.data.token);
         // memperbarui reducer
         dispatch(loginAction(res.data));
@@ -70,68 +78,67 @@ function Navbar(props) {
 
   return (
     <div>
-
-    <div className="px-10 md:px-32 lg:px-48 xl:px-80 bg-primary sticky top-0 left-0 right-0 z-10 border-b-4 border-double border-neutral-100">
-      <div className="navbar">
-        <div className="flex-1">
-          <Link href="/">
-            <div className="btn btn-ghost gap-2">
-              <Image
-                src={require("../../assets/logo.png")}
-                width="40px"
-                height="40px"
-                alt="logo"
-              />
-              <a className="normal-case text-3xl font-bold">Kartoffel</a>
+      <div className="px-10 md:px-32 lg:px-48 xl:px-80 bg-primary sticky top-0 left-0 right-0 z-10 border-b-4 border-double border-neutral-100">
+        <div className="navbar">
+          <div className="flex-1">
+            <Link href="/">
+              <div className="btn btn-ghost gap-2">
+                <Image
+                  src={require("../../assets/logo.png")}
+                  width="40px"
+                  height="40px"
+                  alt="logo"
+                />
+                <a className="normal-case text-3xl font-bold">Kartoffel</a>
+              </div>
+            </Link>
+          </div>
+          <div className="flex-none gap-2">
+            {user.verified_status === 0 && (
+              <>
+                <div onClick={() => setVisible(!visible)}>
+                  <AddButton />
+                </div>
+                <ModalCreatePost
+                  visible={visible}
+                  toggleVisible={() => setVisible(!visible)}
+                />
+              </>
+            )}
+            <div className="dropdown dropdown-end">
+              <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src={`${API_URL}${user.profile_picture}`} />
+                </div>
+              </label>
+              <ul
+                tabIndex="0"
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-secondary rounded-box w-52"
+              >
+                {user.verified_status === 0 && (
+                  <>
+                    <Link href={`/profile?id=${user.id}`}>
+                      <li>
+                        <a className="justify-between">Profile</a>
+                      </li>
+                    </Link>
+                    <Link href="/profile/settings">
+                      <li>
+                        <a>Setting</a>
+                      </li>
+                    </Link>
+                  </>
+                )}
+                <Link href="/auth/login">
+                  <li>
+                    <a onClick={handleLogout}>Logout</a>
+                  </li>
+                </Link>
+              </ul>
             </div>
-          </Link>
-        </div>
-        <div className="flex-none gap-2">
-          {user.verified_status === 0 && (
-            <>
-              <div onClick={() => setVisible(!visible)}>
-                <AddButton />
-              </div>
-              <ModalCreatePost
-                visible={visible}
-                toggleVisible={() => setVisible(!visible)}
-              />
-            </>
-          )}
-          <div className="dropdown dropdown-end">
-            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img src={`${API_URL}${user.profile_picture}`} />
-              </div>
-            </label>
-            <ul
-              tabIndex="0"
-              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-secondary rounded-box w-52"
-            >
-              {user.verified_status === 0 && (
-                <>
-                  <Link href={`/profile?id=${user.id}`}>
-                    <li>
-                      <a className="justify-between">Profile</a>
-                    </li>
-                  </Link>
-                  <Link href="/profile/settings">
-                    <li>
-                      <a>Setting</a>
-                    </li>
-                  </Link>
-                </>
-              )}
-              <Link href="/auth/login">
-                <li>
-                  <a onClick={handleLogout}>Logout</a>
-                </li>
-              </Link>
-            </ul>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
