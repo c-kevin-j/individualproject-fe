2f690e6ec083651094efbd48bdfc925626cd6856
c-kevin-j <kevin_julius@srwasia.com>
2022-07-06 20:25:35 +0700

    adjust positiioning

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -2,12 +2,12 @@ import Navbar from "../Components/Navbar";
 
 function Layout1 ({children}) {
   return <div className="layout1">
-    <div className="sticky top-0 left-0 right-0 z-10 ">
+    <div className="fixed top-0 left-0 right-0 z-10 ">
     <Navbar />
 
     </div>
     <>
-    <div className="overflow-y-auto max-h-screen h-fit">
+    <div className="relative top-20 max-h-screen h-fit">
       {children}
     </div>
     </>  
