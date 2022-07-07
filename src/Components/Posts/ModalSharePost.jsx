ce968463d9c2c7f0d2328339ff0a53ef637a3bc5
c-kevin-j <kevin_julius@srwasia.com>
2022-07-07 20:15:57 +0700

    add notification after copy url

--- src/Components/Posts/ModalSharePost.jsx
+++ src/Components/Posts/ModalSharePost.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal } from "react-daisyui";
 import { AiFillCopy } from "react-icons/ai";
 import { TiThumbsOk } from "react-icons/ti";
@@ -13,12 +13,20 @@ import {
 
 const ModalSharePost = (props) => {
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(props.shareUrl)
+    navigator.clipboard.writeText(props.shareUrl);
+    setNotifVisible(true);
+    setTimeout(() => {
+      setNotifVisible(false);
+    }, 3000);
   };
+
+  const [notifVisible, setNotifVisible] = useState(false);
+  const handleButtonCopy = () => {};
+
   return (
     <>
       <Modal
-        className="bg-base-300 p-3 w-1/2 lg:w-1/3 rounded space-y-3"
+        className="bg-base-300 p-3 pb-8 w-1/2 lg:w-1/3 rounded space-y-3"
         open={props.visible}
         onClickBackdrop={props.toggleVisible}
       >
@@ -40,13 +48,21 @@ const ModalSharePost = (props) => {
             value={props.shareUrl}
             readonly
           />
-          <button type="button" className="bg-base-200" onClick={copyToClipboard}>
-            <AiFillCopy className="w-10 "/>
+          <button
+            type="button"
+            className="bg-base-200"
+            onClick={copyToClipboard}
+          >
+            <AiFillCopy className="w-10 " />
           </button>
         </div>
-        {/* <div className="flex h-3 relative -top-3 italic text-xs">
-         <TiThumbsOk size={40}/> url copied
-        </div> */}
+        <div className="flex h-2 relative -top-3 italic text-xs justify-end">
+          {notifVisible && (
+            <>
+              <TiThumbsOk size={40} /> url copied
+            </>
+          )}
+        </div>
       </Modal>
     </>
   );
